fc46fac3c60e47fc11053bcb2382e2a052327f14
khaledgg <khaber.eg@gmail.com>
2023-01-02 23:39:53 -0500

    restarting project

--- low_level_control/motor_control/motor_control.ino
+++ low_level_control/motor_control/motor_control.ino
@@ -1,8 +1,15 @@
-// ledPin refers to ESP32 GPIO 
+/*
+Stepper, stepper, servo
+*/
+#include <ESP32Servo.h>
+
+Servo myServo;
+//Servo Setup
+
 
 void setup() {
   // put your setup code here, to run once:
-  int a;
+
 }
 
 void loop() {
